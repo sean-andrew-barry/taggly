e3c52bcf2006a9aa594349d8cd60a2713668a656
Sean Barry <sean.barry.developer@gmail.com>
2025-04-20 10:19:20 -0600

    Formatting

--- private/js/External/CountBufferLines.js
+++ private/js/External/CountBufferLines.js
@@ -1,17 +1,13 @@
-  // NOTE: Credit to SO user Emil Vikström at https://stackoverflow.com/a/41439945
-  // for this handly bit of code
-  // Thank you!
-  export function CountBufferLines(buffer)
-  {
-    let index = -1;
-    let line_count = -1;
+// NOTE: Credit to SO user Emil Vikström at https://stackoverflow.com/a/41439945 for this handy bit of code
+// Thank you!
+export function CountBufferLines(buffer) {
+  let index = -1;
+  let line_count = -1;
 
-    do
-    {
-      index = buffer.indexOf(10, index + 1);
-      line_count++;
-    }
-    while (index !== -1);
+  do {
+    index = buffer.indexOf(10, index + 1);
+    line_count++;
+  } while (index !== -1);
 
-    return line_count;
-  }
+  return line_count;
+}
