89042ed2e36e5b1adbac366f950a906391176fd9
Sean Barry <sean.barry.developer@gmail.com>
2025-04-20 10:17:14 -0600

    Using the Preloader instead of the old Loader

--- Start.js
+++ Start.js
@@ -1,4 +1,13 @@
 import { registerHooks } from "node:module";
-import { resolve, load } from "./Loader.js";
+import { initialize, resolveSync as resolve, loadSync as load } from "./Preloader.js";
 
 registerHooks({ resolve, load });
+
+// const mod = await import("/js/Loader.js");
+
+const data = JSON.parse(process.env.TAGGLY_PACKAGE);
+data.preloads = new Map();
+
+await initialize(data);
+
+// console.log(mod);
